dc08b8cb407f13852e374f9c80ef8b6d0ec3ec04
Faraz Shariff <21FShariff@heckgrammar.co.uk>
2025-01-16 12:20:34 +0000

    began code

--- Program.cs
+++ Program.cs
@@ -1,25 +1,71 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace compare_algorithm
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Random n = new Random();
-            Stopwatch sw = new Stopwatch();
-
-
-        }
-        static int[] CreateArray(int size, Random r)
+        public static void Main(string[] args)
         {
             
+            
+            Random n = new Random();
+            Stopwatch sw = new Stopwatch();
+            Console.WriteLine("welcome, how big do you want your array to be");
+            int Size = Convert.ToInt32(Console.ReadLine());
+            int[] array = CreateArray(Size, n);
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            menu(array);
+            
         }
-        static void menu()
+        public static int[] CreateArray(int size, Random r)
         {
+           int[]  array = new int[size];
+            for (int k = 0; k < array.Length; k++)
+            {
+                array[k] = r.Next(0, 100);
+            }
+            Console.WriteLine("great! the array has been created using numbers 1 - 100");
+            return array;
+        }
+        static void menu(int[] array)
+        {
+            Console.WriteLine("using your array, would you like to");
+            Console.WriteLine("1) linear search");
+            Console.WriteLine("2) binary search");
+            Console.WriteLine("3) bubble sort");
+            Console.WriteLine("4) merge sort");
+            Console.WriteLine("5) quit");
+            int UserAnswer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(UserAnswer);
+            if (UserAnswer == 1)
+            {
+                Console.WriteLine("and what number would you like to find");
+                int NumToFind = Convert.ToInt32(Console.ReadLine());
+                LinearSearch(array, NumToFind);
+            }
+            else if (UserAnswer == 2)
+            {
+                Console.WriteLine("and what number would you like to find");
+                int NumToFind = Convert.ToInt32(Console.ReadLine());
+                BinarySearch(array, NumToFind);
+            }
+            else if (UserAnswer == 3)
+            {
+                BubbleSort(array);
+            }
+            else if (UserAnswer == 4) 
+            {
+                MergeSortRecursive(array, array[0], array[1]);
+            }
+          
 
         }
-        
+
         static void BubbleSort(int[] a)
         {
             
@@ -80,11 +126,11 @@ namespace compare_algorithm
         }
         static bool LinearSearch(int[] a, int numToFind)
         {
-
+            return LinearSearch(a, numToFind);
         }
         static bool BinarySearch(int[] a, int numToFind)
         {
-            
+            return BinarySearch(a, numToFind);
         }
             
     }
